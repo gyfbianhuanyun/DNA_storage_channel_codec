1d7ba59b476ddb8de309f3fb7054bec5951500c5
gyfbianhuanyun <44255114+gyfbianhuanyun@users.noreply.github.com>
2022-08-26 17:27:18 +0900

    Add encoder and decoder

---
+++ codec/decoder.py
@@ -0,0 +1,47 @@
+from codec_map import Decode_Map_d2b
+
+
+def decoder_d2b(dna_data, homopolymer=3, codec_map=Decode_Map_d2b):
+    binary_seq = []
+
+    # Initial state
+    homopolymer_count = 1
+    check_base = None
+
+    # Read data from DNA list
+    for i in range(0, len(dna_data)):
+        for j in range(0, len(dna_data[i])):
+            # Determine whether the homopolymer constraints are met
+            if homopolymer_count < homopolymer:
+                # Convert DNA to binary
+                dna_symbol = dna_data[i][j]
+                present_bits = codec_map[dna_symbol]
+                binary_seq.append(present_bits)
+
+                # Check homopolymer count
+                if check_base == dna_symbol:
+                    homopolymer_count += 1
+                else:
+                    # Restore the initial state and change the checking base
+                    homopolymer_count = 1
+                    check_base = dna_symbol
+
+            # Homopolymer decoding
+            else:
+                # If homopolymer constraints are met, read next base of DNA data
+                if check_base == 'A' or check_base == 'T':
+                    dna_symbol = 'A' + dna_data[i][j]
+                else:
+                    dna_symbol = 'C' + dna_data[i][j]
+
+                # Convert by codec map
+                present_bits = codec_map[dna_symbol]
+                # Restore the initial state and change the checking base
+                check_base = dna_data[i][j]
+                homopolymer_count = 1
+                binary_seq.append(present_bits)
+
+        homopolymer_count = 1
+        check_base = None
+
+    return binary_seq
